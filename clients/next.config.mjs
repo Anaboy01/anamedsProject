73f0ee7dd709d2d573f58cc77ef2f6f865c3f685
Anate <aliyuanate016@gmail.com>
2024-09-12 15:58:49 +0100

    lick

--- clients/next.config.mjs
+++ clients/next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    reactStrictMode: true,
+    images: {
+      domains: ['res.cloudinary.com'], // Add Cloudinary as an allowed domain
+    },
+};
 
 export default nextConfig;
