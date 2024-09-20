ff069c6344cec24813182ef66ec4c21dae855ed1
muhammedola <141998553+muhammedola@users.noreply.github.com>
2024-09-20 23:07:02 +0100

    update

--- clients/src/app/doctor/dashboard/page.jsx
+++ clients/src/app/doctor/dashboard/page.jsx
@@ -1,19 +1,22 @@
-'use client'
+"use client";
 import { useState } from "react";
+
 import Sidebar from "../../components/Sidebar/Sidebar";
-
 import { BiPlusMedical } from "react-icons/bi";
-import { BsFileMedicalFill } from "react-icons/bs";
-import { BsJournalMedical } from "react-icons/bs";
-import Link from "next/link";
+import { BsFileMedicalFill, BsJournalMedical } from "react-icons/bs";
+import ReusableModal from "@/app/components/Modal/Modal";
+import { useRouter } from "next/navigation";
+import Layout from "@/app/components/Layout/Layout";
 
-export default function page() {
+
+export default function Page() {
   const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [selectedRecordType, setSelectedRecordType] = useState("");
+  const [inputValue, setInputValue] = useState("");
+  const router = useRouter();
+
   const initialState = {
-    // firstName: user?.firstName || "",
-    // lastName: user?.lastName || "",
-    // photo: user?.photo || "",
-    // role: user?.role || "",
     firstName: "Muhammed",
     lastName: "Musa",
     photo: "",
@@ -21,62 +24,103 @@ export default function page() {
   };
 
   const [profile, setProfile] = useState(initialState);
+
   const toggleSidebar = () => {
     setIsSidebarOpen(!isSidebarOpen);
   };
+
+  const openModal = (recordType) => {
+    setSelectedRecordType(recordType);
+    setIsModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setIsModalOpen(false);
+    setInputValue("");
+  };
+
+  const handleInputChange = (e) => {
+    setInputValue(e.target.value);
+  };
+
+  const handleSubmit = (email) => {
+    switch (selectedRecordType) {
+      case "Create new medical record":
+        router.push(`/doctor/create-record/${email}`);
+        break;
+      case "Create new journal record":
+        router.push(`/patient-files?email=${email}`);
+        break;
+      case "Create new file record":
+        router.push(`/created-files?email=${email}`);
+        break;
+      default:
+        console.error("Unknown record type");
+    }
+    closeModal();
+  };
+
   return (
-    <div className="flex flex-col md:flex-row min-h-screen">
-      <Sidebar
-        isSidebarOpen={isSidebarOpen}
-        toggleSidebar={toggleSidebar}
-        profile={profile}
-        // router={location}
-      />
-
-      <div className=" px-[69px] text-black pt-[74px] flex flex-col  gap-[80px]">
-
-      <h6 className="text-[#3D3D3D] font-medium text-2xl ">Welcome to your Dashboard Dr. Dave</h6>
-
-      <div className="flex flex-row w-full items-center gap-[36px]">
-        <Link href="#" className="w-[229px] h-[101px]  rounded-[20px] border-[2px] border-[#BDBDBD] cursor-pointer">
-            
-            <div className=" flex items-center justify-center h-[48px] bg-[#F9F6F9] rounded-tl-[20px] rounded-tr-[20px] ">
-            <BiPlusMedical  className="h-[24px] w-[24px] text-[#374957]" />
+   <Layout>
+      <div className="flex flex-col md:flex-row min-h-screen">
+        <div className="px-[69px] text-black pt-[74px] flex flex-col gap-[80px]">
+          <h6 className="text-[#3D3D3D] font-medium text-2xl">
+            Welcome to your Dashboard Dr. Dave
+          </h6>
+  
+          <div className="flex flex-row w-full items-center gap-[36px]">
+            <div
+              onClick={() => openModal("Create new medical record")}
+              className="w-[229px] h-[101px] rounded-[20px] border-[2px] border-[#BDBDBD] cursor-pointer"
+            >
+              <div className="flex items-center justify-center h-[48px] bg-[#F9F6F9] rounded-tl-[20px] rounded-tr-[20px]">
+                <BiPlusMedical className="h-[24px] w-[24px] text-[#374957]" />
+              </div>
+              <div className="text-center h-[49px] bg-[#C2E5F5] py-[14px] font-normal text-[15px] rounded-br-[20px] rounded-bl-[20px]">
+                Create new record
+              </div>
             </div>
-          
-
-            <div className="text-center h-[49px] bg-[#C2E5F5] py-[14px] font-normal text-[15px]  rounded-br-[20px] rounded-bl-[20px]">
-            Create new record
+  
+            <div
+              onClick={() => openModal("Create new journal record")}
+              className="w-[229px] h-[101px] rounded-[20px] border-[2px] border-[#BDBDBD] cursor-pointer"
+            >
+              <div className="flex items-center justify-center h-[48px] bg-[#F9F6F9] rounded-tl-[20px] rounded-tr-[20px]">
+                <BsJournalMedical className="h-[24px] w-[24px] text-[#374957]" />
+              </div>
+              <div className="text-center h-[49px] bg-[#EBDFEB] py-[14px] font-normal text-[15px] rounded-br-[20px] rounded-bl-[20px]">
+                Access patient files
+              </div>
             </div>
-        </Link>
-        <Link href="#" className="w-[229px] h-[101px]  rounded-[20px] border-[2px] border-[#BDBDBD] cursor-pointer">
-
-            <div className=" flex items-center justify-center h-[48px] bg-[#F9F6F9] rounded-tl-[20px] rounded-tr-[20px]">
-            <BsJournalMedical  className="h-[24px] w-[24px] text-[#374957]" />
+  
+            <div
+              onClick={() => openModal("Create new file record")}
+              className="w-[229px] h-[101px] rounded-[20px] border-[2px] border-[#BDBDBD] cursor-pointer"
+            >
+              <div className="flex items-center justify-center h-[48px] bg-[#F9F6F9] rounded-tl-[20px] rounded-tr-[20px]">
+                <BsFileMedicalFill className="h-[24px] w-[24px] text-[#374957]" />
+              </div>
+              <div className="text-center h-[49px] bg-[#EBDFEB] py-[14px] font-normal text-[15px] rounded-br-[20px] rounded-bl-[20px]">
+                Access files you created
+              </div>
             </div>
-          
-
-            <div className="text-center h-[49px] bg-[#EBDFEB] py-[14px] font-normal text-[15px]  rounded-br-[20px] rounded-bl-[20px]">
-            Create new record
-            </div>
-        </Link>
-        <Link href="#" className="w-[229px] h-[101px]  rounded-[20px] border-[2px] border-[#BDBDBD] cursor-pointer">
-
-            <div className=" flex items-center justify-center h-[48px] bg-[#F9F6F9] rounded-tl-[20px] rounded-tr-[20px]">
-            <BsFileMedicalFill  className="h-[24px] w-[24px] text-[#374957]" />
-            </div>
-          
-
-            <div className="text-center h-[49px] bg-[#EBDFEB] py-[14px] font-normal text-[15px]  rounded-br-[20px] rounded-bl-[20px]">
-            Create new record
-            </div>
-        </Link>
+          </div>
+        </div>
+  
+        {isModalOpen && (
+          <ReusableModal
+            title="Patient Email"
+            description="Please enter the patient's Email to proceed."
+            placeholder="Joe@Example.com"
+            show={isModalOpen}
+            type="email"
+            onClose={closeModal}
+            handleSubmit={handleSubmit}
+            inputValue={inputValue}
+            handleInputChange={handleInputChange}
+          />
+        )}
       </div>
-
-
-        
-      </div>
-      
-    </div>
-  )
+   </Layout>
+  );
 }
--- clients/src/app/doctor/login/page.jsx
+++ clients/src/app/doctor/login/page.jsx
@@ -1,40 +1,129 @@
-"use client";
-import Image from "next/image";
-import Link from "next/link";
+"use client"
+import { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useRouter } from "next/navigation";
+import { toast } from "react-toastify";
+import {
+  loginDoctor,
+  RESET,
+} from "@/app/redux/features/doctorsApi/doctorsSlice";
 import { Form } from "./form";
 import Nav from "../../components/Nav";
-import Inter from '../../../../public/userinter.svg'
+import Image from "next/image";
+import Link from "next/link";
+import Inter from "../../../../public/userinter.svg";
 
-export default function page() {
+const initialState = {
+  email: "",
+  password: "",
+};
+
+// Add this function to validate email
+const validateEmail = (email) => {
+  return String(email)
+    .toLowerCase()
+    .match(
+      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+    );
+};
+
+export default function LoginPage() {
+  const [formData, setFormData] = useState(initialState);
+  const { email, password } = formData;
+  const dispatch = useDispatch();
+  const router = useRouter();
+
+  const { isError, isLoading, isLoggedIn, isSuccess } = useSelector(
+    (state) => state.doctor
+  );
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const loginDoctors = async (e) => {
+    e.preventDefault();
+
+    if (!email || !password) {
+      return toast.error("All fields are required");
+    }
+
+    if (!validateEmail(email)) {
+      return toast.error("Please enter a valid email");
+    }
+
+    const doctorData = {
+      email,
+      password,
+    };
+
+    const toastId = toast.loading("Logging in...");
+
+    try {
+      await dispatch(loginDoctor(doctorData)).unwrap();
+      toast.update(toastId, {
+        render: "Logged in successfully",
+        type: "success",
+        isLoading: false,
+        autoClose: 3000,
+      });
+    } catch (error) {
+      toast.update(toastId, {
+        render: "Login failed. Please check your credentials.",
+        type: "error",
+        isLoading: false,
+        autoClose: 3000,
+      });
+      console.error(error)
+    }
+  };
+
+  useEffect(() => {
+    if (isSuccess && isLoggedIn) {
+      router.push("/doctor/dashboard");
+    }
+
+    dispatch(RESET());
+  }, [isLoggedIn, isSuccess, dispatch, router]);
+
   return (
     <main>
       <Nav />
-      <div className="h-screen flex justify-center items-center gap-[174px] w-full text-black">
+      <div className="h-screen flex gap-[174px] justify-center items-center text-black">
         <div className="px-8 pb-8 pt-4 rounded-xl space-y-8">
-          <h1 className="font-semibold text-2xl font-montserrat">Login</h1>
-          <Form />
+          <div className="flex flex-col gap-[7px]">
+            <h1 className="font-semibold text-2xl font-montserrat">Login</h1>
+            <p className="text-[#888888] font-poppins">
+              Don't have an account?{" "}
+              <Link href="/register" className="text-[#2AA0CD] hover:underline">
+                Sign up.
+              </Link>
+            </p>
+          </div>
+
+          <Form
+            formData={formData}
+            handleInputChange={handleInputChange}
+            loginDoctor={loginDoctors}
+            error={isError ? "Invalid email or password" : ""}
+            loading={isLoading}
+          />
         </div>
 
-        <div className="bg-oval bg-center bg-90 bg-no-repeat flex justify-center items-center h-screen">
-          <div className=" bg-cover lg:block w-[80%]">
-            <div className=" flex items-center flex-col justify-center px-5 py-5 text-center from-[#C8E6C] font-lato to-[#B3E5FC]">
+        <div className="bg-oval bg-90 p-[10px] bg-center bg-no-repeat flex justify-center items-center h-screen text-center">
+          <div className="lg:block p-[10px]">
+            <div className="flex items-center flex-col justify-center px-5 bg-gradient-to-r from-[#C8E6C] to-[#B3E5FC]">
               <div>
-                <h2 className="text-[24px] font-bold text-[#00579B] font-lato sm:text-[24px]">
-                Hey, Doc! Log in to manage patient files with ease!
-
+                <h2 className="text-2xl font-bold text-[#00579B] sm:text-[24px] font-lato">
+                  Hey, Doc! Log in to manage patient files with ease!
                 </h2>
 
-
-                <p className="max-w-xl font-normal text-[16px] mt-3 text-[#0000008A]">
+                <p className="max-w-xl mt-3 text-[#0000008A] font-lato">
                   Join our community to help support patients in need of financial assistance for their health care.
                 </p>
               </div>
-              <Image
-                src={Inter}
-                width={294.21}
-                height={248.85}
-                alt=''
-              />
+              <Image src={Inter} width={294.21} height={248.85} alt="" />
             </div>
           </div>
         </div>
